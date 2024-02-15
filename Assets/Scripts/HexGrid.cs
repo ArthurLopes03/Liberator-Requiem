d464b96e081f083757b4951e1bc680d2602cdf31
ArthurLopes03 <C22509166@mytudublin.ie>
2024-02-15 21:06:26 +0000

    Elevation added to tiles
    There is no god

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,49 +1,47 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class HexGrid : MonoBehaviour
-{
+public class HexGrid : MonoBehaviour {
 
 	public int width = 6;
 	public int height = 6;
 
+	public Color defaultColor = Color.white;
+
 	public HexCell cellPrefab;
-
 	public Text cellLabelPrefab;
-
-	Canvas gridCanvas;
 
 	HexCell[] cells;
 
+	Canvas gridCanvas;
 	HexMesh hexMesh;
 
-	public Color defaultColor = Color.white;
-	public Color touchedColor = Color.magenta;
-
-
-	void Awake()
-	{
+	void Awake () {
 		gridCanvas = GetComponentInChildren<Canvas>();
 		hexMesh = GetComponentInChildren<HexMesh>();
 
 		cells = new HexCell[height * width];
 
-		for (int z = 0, i = 0; z < height; z++)
-		{
-			for (int x = 0; x < width; x++)
-			{
+		for (int z = 0, i = 0; z < height; z++) {
+			for (int x = 0; x < width; x++) {
 				CreateCell(x, z, i++);
 			}
 		}
 	}
 
-	void Start()
-	{
+	void Start () {
 		hexMesh.Triangulate(cells);
 	}
 
-	void CreateCell(int x, int z, int i)
-	{
+    public HexCell GetCell(Vector3 position)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        return cells[index];
+    }
+
+    void CreateCell (int x, int z, int i) {
 		Vector3 position;
 		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
 		position.y = 0f;
@@ -53,6 +51,25 @@ public class HexGrid : MonoBehaviour
 		cell.transform.SetParent(transform, false);
 		cell.transform.localPosition = position;
 		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+		cell.color = defaultColor;
+
+		if (x > 0) {
+			cell.SetNeighbor(HexDirection.W, cells[i - 1]);
+		}
+		if (z > 0) {
+			if ((z & 1) == 0) {
+				cell.SetNeighbor(HexDirection.SE, cells[i - width]);
+				if (x > 0) {
+					cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
+				}
+			}
+			else {
+				cell.SetNeighbor(HexDirection.SW, cells[i - width]);
+				if (x < width - 1) {
+					cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
+				}
+			}
+		}
 
 		Text label = Instantiate<Text>(cellLabelPrefab);
 		label.rectTransform.SetParent(gridCanvas.transform, false);
@@ -60,35 +77,11 @@ public class HexGrid : MonoBehaviour
 			new Vector2(position.x, position.z);
 		label.text = cell.coordinates.ToStringOnSeparateLines();
 
-		cell.color = defaultColor;
-	}
+        cell.uiRect = label.rectTransform;
+    }
 
-	void Update()
-	{
-		if (Input.GetMouseButton(0))
-		{
-			HandleInput();
-		}
-	}
-
-	void HandleInput()
-	{
-		Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
-		if (Physics.Raycast(inputRay, out hit))
-		{
-			TouchCell(hit.point);
-		}
-	}
-
-	void TouchCell(Vector3 position)
-	{
-		position = transform.InverseTransformPoint(position);
-		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
-		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
-		HexCell cell = cells[index];
-		cell.color = touchedColor;
-		hexMesh.Triangulate(cells);
-		Debug.Log("touched at " + coordinates.ToString());
-	}
+    public void Refresh()
+    {
+        hexMesh.Triangulate(cells);
+    }
 }
