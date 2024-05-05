7416d1c64117bc8594ea7e6bfe8af12d2b0dcfb5
simojian <simon.kondela@gmail.com>
2024-05-05 20:50:58 +0100

    healthbar damage highlighting linked

--- Assets/Scripts/InGameUI/healthManager.cs
+++ Assets/Scripts/InGameUI/healthManager.cs
@@ -1,28 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class healthManager : MonoBehaviour
 {
-    private HexUnit unit;
-    private Material mat;
+    private HexUnit _unit;
+    private Material _mat;
+    private float _incommingDamage;
+
+    private HexGameUI _uiManager;
 
     public float maxHealth = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        unit = gameObject.GetComponentInParent<HexUnit>();
-        mat = gameObject.GetComponent<Renderer>().material;
+        _unit = gameObject.GetComponentInParent<HexUnit>();
+        _mat = gameObject.GetComponent<Renderer>().material;
 
-        maxHealth = unit.health;
+        _uiManager = FindAnyObjectByType<HexGameUI>(FindObjectsInactive.Include).GetComponent<HexGameUI>();
+
+        _incommingDamage = 0f;
+
+        maxHealth = _unit.health;
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
-        
-        mat.SetFloat("_MaxHealth", maxHealth);
-        mat.SetFloat("_Health", unit.health);
+
+        CheckForHighlightDamage();
+
+        _mat.SetFloat("_MaxHealth", maxHealth);
+        _mat.SetFloat("_Health", _unit.health);
+        _mat.SetFloat("_ShowDamage", _incommingDamage);
+    }
+
+    void CheckForHighlightDamage()
+    {
+        if (_uiManager.selectedUnit != null)
+        {
+
+
+            if (_uiManager.currentCell.Unit == _unit && _uiManager.selectedUnit.gameObject.tag != _unit.gameObject.tag)
+            {
+
+                _incommingDamage = _uiManager.selectedUnit.attackPow;
+
+            }
+            else
+            {
+                _incommingDamage = 0;
+            }
+        }
+        else
+        {
+            _incommingDamage = 0;
+        }
     }
 }
